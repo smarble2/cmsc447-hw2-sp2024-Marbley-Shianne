3d486841f68833e6117bc9b1b5bd20cde6275f69
shiannemarbles <shiannemarbles@gmail.com>
2024-02-20 16:07:13 -0500

    working  >python create terminal

--- tables.py
+++ tables.py
@@ -8,7 +8,8 @@ import sqlite3
 connected = sqlite3.connect('database.db') #connects to database.db file
 print("database.db connected successfully") #print statemnet for confirm
 
-connected.execute('CREATE TABLE info (name TEXT, iden TEXT, points TEXT)')
+connected.execute('CREATE TABLE info (name TEXT, id TEXT, points TEXT)')
+#connected.execute('DROP TABLE info') 
 print("Table created successfully")#print statemnet for confirm
 
 connected.close() #close connection
